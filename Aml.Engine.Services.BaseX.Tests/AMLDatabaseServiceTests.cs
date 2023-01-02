8046fb399dd30e5374d0c69363413009f1f86ef3
Josef <josef.prinz@inpro.de>
2023-01-02 16:36:36 +0100

    Add project files.

---
+++ Aml.Engine.Services.BaseX.Tests/AMLDatabaseServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aml.Engine.Services.BaseX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aml.Engine.Services.BaseX.Tests
+{
+    [TestClass()]
+    public class AMLDatabaseServiceTests
+    {
+        [TestMethod()]
+        public void ConnectTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            var service = AMLDatabaseService.Register();
+            Assert.IsNotNull(service);
+            Assert.IsNotNull(Aml.Engine.Services.ServiceLocator.GetService<AMLDatabaseService>());
+        }
+
+        [TestMethod()]
+        public void UnRegisterTest()
+        {
+            AMLDatabaseService.UnRegister(); 
+            Assert.IsNull(Aml.Engine.Services.ServiceLocator.GetService<AMLDatabaseService>());
+        }
+    }
+}
---
+++ Aml.Engine.Services.BaseX/AMLDatabaseService.cs
@@ -0,0 +1,40 @@
+﻿using Aml.Engine.Services.Interfaces;
+
+namespace Aml.Engine.Services.BaseX
+{
+    /// <summary>
+    /// This service provides methods to connect to an AutomationML BaseX database
+    /// and to query model data from the database.
+    /// </summary>
+    public class AMLDatabaseService: IAMLService
+    {
+        /// <summary>
+        /// Establish a database connection
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public bool Connect (string host, string port)
+        {
+            return false;
+        }
+
+        private AMLDatabaseService () { }
+
+        public static AMLDatabaseService Register ()
+        {
+            var service = new AMLDatabaseService ();    
+            ServiceLocator.Register (service);
+            return service;
+        }
+
+        public static void UnRegister ()
+        {
+            var service = ServiceLocator.GetService<AMLDatabaseService> ();
+            if (service != null)
+            { 
+                ServiceLocator.UnRegister<AMLDatabaseService>();
+            }
+        }
+    }
+}
